aad4e8a58992d2f84ed438b25e315050e6dfb9dc
LockT <72506238+lckt0@users.noreply.github.com>
2022-04-15 01:12:56 +0300

    Update discord.php

--- discord.php
+++ discord.php
@@ -1,1 +1,18 @@
-
+<?php
+  $json = file_get_contents('https://discord.com/api/guilds/852431438413561866/widget.json');
+  $obj = json_decode($json);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $obj->name; ?></title>
+</head>
+<body>
+    <script>
+        window.location.replace("<?php echo $obj->instant_invite; ?>");
+    </script>
+</body>
+</html>
